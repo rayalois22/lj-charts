2c5ba5e43d603e9c4d2cd227120ba2d4c4bda092
Jude Kikuyu <jkikuyu@gmail.com>
2017-08-12 10:19:19 +0300

    initial commit

---
+++ lang/en.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang["title"] = "Welcome to Habari Leo";
+$lang["personal_info"] = "Personal Information";
+$lang["username"] = "Username";
+$lang["fullname"] = "Full Name";
+
+$lang["password"] = "Password";
+$lang["email"] = "Email";
+$lang["phonenumber"] = "Phone Number";
+$lang["profile_image"]="Profile Image";
+$lang["returnbutton"] = "Return";
+$lang["updatedetailsbutton"] = "Update Record";
+$lang["savedetailsbutton"] = "Save Details";
+$lang["savedetails"] = "Save Details";
+$lang["movetoadmin"] = "Move this area to an administrator's page";
+$lang["copyright"] = "Copyright &copy; " . date("Y") . " - BBIT3104";
+$lang["table_name"] = $_SESSION["table_name"];
+$lang["group_name"] = ucwords(strtolower(str_replace( '_' , ' ' , $lang["table_name"] )));
+$lang["table_title"] = $lang["group_name"] . " Table List";
+
+
+
+?>
