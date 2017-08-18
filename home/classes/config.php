63eb7807b3a3378607ef4d4d9431a1b23ce845ad
Alois Odhiambo <rayalois22@gmail.com>
2017-08-19 02:22:19 +0300

    Update config.php

--- home/classes/config.php
+++ home/classes/config.php
@@ -4,7 +4,8 @@
 			$this->db_params();
 		}
 		private function db_params(){
-			$db_args = array("localhost", "root", "root", "lj_charts_db");
+			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+			$db_args = array($url["host"], $url["user"], $url["pass"], substr($url["path"], 1));
 			$db_fields = array("HOST", "USER", "PASS", "DATABASE");
 			$db_args = array_combine($db_fields, $db_args);
 			define("DB_PARAMS", $db_args);
